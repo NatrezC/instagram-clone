dffb50ab7471cc4fa462b24e7b2e94ef8158c9eb
NatrezC <cnatrez@gmail.com>
2021-05-15 08:29:51 -0500

    created setpost

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, { useState } from 'react';
 import './App.css';
 
 //import Components
 import Post from './components/Post'
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "staytuned96",
+        caption: "Wow it works", 
+        imageUrl: "/images/reactpic.png"
+    },
+    {
+      username: "staytuned96",
+        caption: "Wow it works", 
+        imageUrl: "/images/reactpic.png"
+    }
+  ]);
   return (
     <div className="app">
       {/*Header*/}
@@ -17,8 +30,14 @@ function App() {
 
       <h1>Hello Clever Programmers</h1>
 
+      {
+        posts.map(post => {
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}
+        })
+      }
+
       <Post username ="staytuned96"  caption="Wow it works" imageUrl="/images/reactpic.png"/>
-      <Post username ="ajeatslocal"  caption="You are getting good at this" imageUrl="/images/reactpic.png"/>
+      <Post username ="ajeatslocal"  caption="You are getting good at this" imageUrl="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/20190503-delish-pineapple-baked-salmon-horizontal-ehg-450-1557771120.jpg?crop=0.669xw:1.00xh;0.173xw,0&resize=640:*"/>
       <Post />
       <Post />
       {/*Post */}
