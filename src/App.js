24a641d7c8bc177810d89804b015d4fee0ead7eb
NatrezC <cnatrez@gmail.com>
2021-05-16 08:49:36 -0500

    running to a bug with post not rendering

--- src/App.js
+++ src/App.js
@@ -32,16 +32,9 @@ function App() {
 
       {
         posts.map(post => {
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
         })
       }
-
-      <Post username ="staytuned96"  caption="Wow it works" imageUrl="/images/reactpic.png"/>
-      <Post username ="ajeatslocal"  caption="You are getting good at this" imageUrl="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/20190503-delish-pineapple-baked-salmon-horizontal-ehg-450-1557771120.jpg?crop=0.669xw:1.00xh;0.173xw,0&resize=640:*"/>
-      <Post />
-      <Post />
-      {/*Post */}
-      {/*Post */}
     </div>
   );
 }
